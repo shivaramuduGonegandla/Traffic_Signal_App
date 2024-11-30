a99fcd9f58f6c71469d6fa37c87abf4f08baf4b6
shivaramudu <shivaramudu2002@gmail.com>
2024-11-30 10:34:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function buttonStop() {
+    document.getElementById("stopLight").style.backgroundColor = "#cf1124";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+
+    document.getElementById("stopButton").style.backgroundColor = "#cf1124";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+
+}
+
+function buttonready() {
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+    document.getElementById("readyLight").style.backgroundColor = "#f7c948";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyButton").style.backgroundColor = "#f7c948";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+
+}
+
+function buttongo() {
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("goLight").style.backgroundColor = "#199473";
+
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goButton").style.backgroundColor = "#199473";
+
+}
